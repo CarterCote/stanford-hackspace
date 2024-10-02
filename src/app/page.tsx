a5570bf28636177257f3139dfca2cc588e0a0c84
cartercote <cartercote06@gmail.com>
2024-10-02 01:02:50 -0700

    added flickering grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,28 @@ import { Button } from "@/components/ui/button";
 import { crimson_regular_italic } from "./fonts";
 import BlurIn from "@/components/ui/blur-in";
 import { RainbowButton } from "@/components/ui/rainbow-button";
+import FlickeringGrid from "@/components/ui/flickering-grid";
+import { useWindowSize } from 'react-use';
 
 export default function Home() {
+  const { width, height } = useWindowSize();
+
   return (
     <div style={{ backgroundImage: 'url(/hackspaceBg.png)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+      <FlickeringGrid
+        className="z-0 absolute inset-0 size-full"
+        squareSize={4}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.5}
+        flickerChance={0.1}
+        height={height}  // Scale down height
+        width={width -15}    // Scale down width
+      />
       <div className="bg-black bg-opacity-40">
         <Header />
         <div
-          className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
+          className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20"
         >
 
           <BlurIn className="flex flex-col gap-8 row-start-2 justify-center items-center sm:items-start text-white">
@@ -33,7 +47,7 @@ export default function Home() {
               </Button>
             </div>
           </BlurIn>
-          <footer className="row-start-3 flex gap-3 flex-wrap items-center justify-center text-white">
+          <footer className="row-start-3 flex flex-wrap items-center justify-center text-white">
             <Link
               className="flex items-center gap-1 no-underline hover:no-underline hover:border-b hover:border-white transition-all duration-300"
               href="https://bases.stanford.edu/"
