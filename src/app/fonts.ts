9862100ee1a2198d4c5c76e3ad9674b9ec07ff60
cartercote <cartercote06@gmail.com>
2024-10-01 17:34:42 -0700

    initial scaffolding

---
+++ src/app/fonts.ts
@@ -0,0 +1,84 @@
+import localFont from "next/font/local";
+import { Plus_Jakarta_Sans, Crimson_Text } from "next/font/google";
+
+export const crimson_regular = Crimson_Text({
+    weight: "400",  // Thin
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const crimson_regular_italic = Crimson_Text({
+    weight: "400",  // Thin
+    style: "italic",
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const crimson_semibold = Crimson_Text({
+    weight: "600",  // Thin
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const crimson_bold = Crimson_Text({
+    weight: "700",  // Thin
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_thin = Plus_Jakarta_Sans({
+    weight: "200",  // Thin
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_thin_italic = Plus_Jakarta_Sans({
+  weight: "200",  // Thin
+  style: "italic",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const plus_jakarta_sans_regular = Plus_Jakarta_Sans({
+    weight: "400",  // Regular
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_regular_italic = Plus_Jakarta_Sans({
+  weight: "400",  // Regular
+  style: "italic",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const plus_jakarta_sans_medium = Plus_Jakarta_Sans({
+    weight: "500",  // Medium
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_semibold = Plus_Jakarta_Sans({
+    weight: "600",  // Semibold
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_semibold_italic = Plus_Jakarta_Sans({
+    weight: "600",  // Semibold
+    style: "italic",
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_bold = Plus_Jakarta_Sans({
+    weight: "700",  // Bold
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const plus_jakarta_sans_extrabold = Plus_Jakarta_Sans({
+    weight: "800",  // Extra Bold
+    subsets: ["latin"],
+    display: "swap",
+});
---
+++ src/app/header.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+    const [countdown, setCountdown] = useState("");
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const now = new Date();
+        const currentDay = now.getDay();
+  
+        if (currentDay >= 5 && currentDay <= 0) {
+          setCountdown("Hackspace is currently live. Stop by and spend some time on ur project!");
+        } else {
+          const friday = new Date(now);
+          friday.setDate(now.getDate() + (5 - currentDay + 7) % 7);
+          friday.setHours(0, 0, 0, 0);
+  
+          const diff = friday.getTime() - now.getTime();
+          const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+          const minutes = Math.floor((diff / (1000 * 60)) % 60);
+          const seconds = Math.floor((diff / 1000) % 60);
+  
+          setCountdown(
+            `${days.toString().padStart(2, "0")} D ${hours
+              .toString()
+              .padStart(2, "0")} H ${minutes
+              .toString()
+              .padStart(2, "0")} M ${seconds.toString().padStart(2, "0")} S until hackspace (aka more time to create)`
+          );
+        }
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+    return (
+      <>
+        <header className="w-full text-white">
+            <div className="flex flex-col justify-between items-center">
+              <div className="flex w-full item-start">
+              </div>
+              <div className="flex md:flex-row flex-col md:pt-6 pt-3 items-center md:px-8 px-2 w-full justify-between">
+                <Link href="/">
+                  <div>
+                    <p className={`text-[18px] hover:bg-clip-text hover:text-transparent hover:bg-gradient-to-r hover:from-[#FFCBA2] hover:to-[#D895B7] transition duration-500`}>stanford hackspace🚢</p>
+                  </div>
+                </Link>
+                <p className={`text-[16px] tracking-tighter`}>{countdown}</p>
+                {/* <Link href="https://groupme.com/join_group/74807389/3z3z3z3z">
+                  <Button
+                    variant="outline" 
+                  >
+                    <div className={`${messina_light.className}`}>
+                      JOIN GROUPME
+                    </div>
+                  </Button>
+                </Link> */}
+              </div>
+            </div>
+        </header>
+      </>
+    );
+}
